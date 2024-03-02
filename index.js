fd3946563214ae1e19be268438dac72f009a1760
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-03-02 16:02:32 +0500

    commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 app.use("/", indexRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
